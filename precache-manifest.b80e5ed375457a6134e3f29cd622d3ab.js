307026da9bd1138960db6461442aefd70ba763da
kartikverma <kvs1297@gmail.com>
2019-11-22 06:00:46 +0530

    made some changes

---
+++ precache-manifest.b80e5ed375457a6134e3f29cd622d3ab.js
@@ -0,0 +1,78 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a6106a32c867e95e87df56628773b325",
+    "url": "/admin/index.html"
+  },
+  {
+    "revision": "e2d49ff23c48ae10ad39",
+    "url": "/admin/static/css/main.1883b9ab.chunk.css"
+  },
+  {
+    "revision": "00deb5262186b34253a5",
+    "url": "/admin/static/js/0.4efa6ecd.chunk.js"
+  },
+  {
+    "revision": "656e6011a317898239b1",
+    "url": "/admin/static/js/1.5784e20d.chunk.js"
+  },
+  {
+    "revision": "0eb72d074288d9aecd42",
+    "url": "/admin/static/js/10.7cd60d2b.chunk.js"
+  },
+  {
+    "revision": "ceeabaf899515b68d40f",
+    "url": "/admin/static/js/11.4face124.chunk.js"
+  },
+  {
+    "revision": "f4f8b081701ce9988c30",
+    "url": "/admin/static/js/12.203d802a.chunk.js"
+  },
+  {
+    "revision": "420eb1ff6edea3966ddf",
+    "url": "/admin/static/js/13.ea9f011a.chunk.js"
+  },
+  {
+    "revision": "151f823f369cfe45ae84",
+    "url": "/admin/static/js/14.409d0f18.chunk.js"
+  },
+  {
+    "revision": "8c1ca8afaf0db4454d14",
+    "url": "/admin/static/js/15.a03e87f5.chunk.js"
+  },
+  {
+    "revision": "2df08b95c09dae39106d",
+    "url": "/admin/static/js/16.0d85d83d.chunk.js"
+  },
+  {
+    "revision": "25d22284e4568cd1d336",
+    "url": "/admin/static/js/4.9732fe50.chunk.js"
+  },
+  {
+    "revision": "7c508365d40fdc534537",
+    "url": "/admin/static/js/5.386c1e6e.chunk.js"
+  },
+  {
+    "revision": "bca6dbe6cd058613ce34",
+    "url": "/admin/static/js/6.1dd9df9e.chunk.js"
+  },
+  {
+    "revision": "51521bb06e92d0f331ab",
+    "url": "/admin/static/js/7.0fe3a8f1.chunk.js"
+  },
+  {
+    "revision": "d6d3f41c3ed9b08b14a9",
+    "url": "/admin/static/js/8.97f5b14c.chunk.js"
+  },
+  {
+    "revision": "cf2f5edc9b3cd9673dc4",
+    "url": "/admin/static/js/9.4e62abbf.chunk.js"
+  },
+  {
+    "revision": "e2d49ff23c48ae10ad39",
+    "url": "/admin/static/js/main.aa7b30a7.chunk.js"
+  },
+  {
+    "revision": "7c9383eb01043d861bb0",
+    "url": "/admin/static/js/runtime-main.a8ef10fd.js"
+  }
+]);
